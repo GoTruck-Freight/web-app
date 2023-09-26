f1bd5bac704068649b9e7da25707dc6c09bd6d19
guliyevdev <q.behruz1@gmail.com>
2023-09-26 17:50:18 +0000

    An attempt was made to resolve the ssl certificate issue

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,18 @@
+const fs = require('fs')
+
 module.exports = {
     devServer: {
       // 'auto' | 'all' [string] here
       allowedHosts: 'all',
-      port: 8000
-    },
+      port: 8000,
+      allowedHosts: 'all',
+      client: {
+	      // Can be `string`:
+	      //
+	      // To get protocol/hostname/port from browser
+	      // webSocketURL: 'auto://0.0.0.0:0/ws'
+	      webSocketURL: 'auto://0.0.0.0:0/ws'
+	},
+     },
   };
+
